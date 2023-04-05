ff8ed02d85ab32e619587283986bbbdced01fdbd
Fernando Nicolás <fersanti2896@gmail.com>
2023-04-05 13:47:26 -0600

    Express validator y custom middleware en validarCampos.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,15 +3,8 @@ const { response } = require('express');
 
 const crearUsuario = (req, res = response) => {
     const { name, email, password } = req.body;
-
-    if( name.length < 5 ) {
-        return res.status(400).json({
-            ok: false,
-            msg: 'El nombre debe de contener 5 letras.'
-        })
-    }
-
-    res.json({
+    
+    res.status(201).json({
         ok: true, 
         msg: 'Registro',
         name,
@@ -22,7 +15,7 @@ const crearUsuario = (req, res = response) => {
 const loginUsuario = (req, res = response) => {
     const { email, password } = req.body;
 
-    res.json({
+    res.status(201).json({
         ok: true, 
         msg: 'Login',
         email,
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,33 @@
 const { Router } = require('express');
 const { crearUsuario, loginUsuario, renovarToken } = require('../controllers/authController');
 const router = Router();
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validarCampos');
 
 /*
     Rutas de Usuarios / Auth
     host + /api/auth
 */
 
-router.post( '/new', crearUsuario );
-router.post( '/', loginUsuario );
+router.post(
+    '/new', 
+    [ check('name', 'El nombre es obligatorio.').not().isEmpty(),
+      check('email', 'El email es obligatorio.').isEmail(), 
+      check('password', 'El password debe contener 8 caracteres.').isLength({ min: 8 }),
+      validarCampos
+    ], 
+    crearUsuario
+);
+
+router.post( 
+    '/', 
+    [ check('email', 'El email es obligatorio.').isEmail(),
+      check('password', 'El password debe contener 8 caracteres.').isLength({ min: 8 }),
+      validarCampos
+    ],
+    loginUsuario 
+);
+
 router.get( '/renew', renovarToken );
 
 module.exports = router;
